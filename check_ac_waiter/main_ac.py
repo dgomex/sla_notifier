ed2fee45ba68ae90bec7bf84f258b52464a359ac
gomes-dani <dgomes@thirtycapital.com>
2023-11-23 06:23:37 +0100

    Add vars to db

--- check_ac_waiter/main_ac.py
+++ check_ac_waiter/main_ac.py
@@ -1,3 +1,5 @@
+import os
+
 from sql_server import SQLServerDatabase
 from notifier import Notifier
 from time import sleep
@@ -6,12 +8,16 @@ from time import sleep
 def main():
     while True:
         try:
-            sql_server = SQLServerDatabase(server="SERVEr_ENDPOINT", username="USERNAME", password="PASSWORD", database="DB")
+            sql_server = SQLServerDatabase(server=os.getenv("AC_SERVER"),
+                                           username=os.getenv("AC_USER"),
+                                           password=os.getenv("AC_PASSWORD"),
+                                           database=os.getenv("AC_DB"))
             query = "SELECT COUNT(1) FROM dbo.idmrunlog WHERE CAST(IdmRunEndTimeUTC as DATE) = CAST(CURRENT_TIMESTAMP AS DATE)"
             print("STARTING QUERY EXECUTION")
             query_result = sql_server.execute_query(query=query)
             print("QUERY COMPLETED")
-            if query_result[0][0] == 1:
+            # if query_result[0][0] == 1:
+            if 1 == 1:
                 print("AC available, calling my master to make something about it")
                 notifier = Notifier()
                 notifier.call()
