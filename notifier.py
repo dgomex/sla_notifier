390938b2c3b330a13564307c937dad671b8fad24
gomes-dani <dgomes@thirtycapital.com>
2023-12-19 23:18:14 +0000

    Add missing parameters

--- notifier.py
+++ notifier.py
@@ -9,19 +9,19 @@ class Notifier:
         self.url = "https://handler.twilio.com/twiml/EH3ab3f9548b34cca8c5f6f2c5bc26f5d9"
         self.phone_number = "+17792329315"
         self.phone_to_call = environ.get("DESTINATION_PHONE_NUMBER", default="+351913558518")
-        self.client = Client()
+        self.twilio_client = Client()
         self.slack_web_client = WebClient(token=environ.get("SLACK_BOT_TOKEN"))
         self.slack_user_monitor_id = environ.get("SLACK_MONITOR_USER_ID")
 
     def call(self):
         print("CREATING CALL")
-        call = self.client.calls.create(
+        call = self.twilio_client.calls.create(
             from_=self.phone_number,
             to=self.phone_to_call,
             url=self.url
         )
         for i in range(30):
-            if self.client.calls.get(sid=call.sid).fetch().status == "busy" or self.client.calls.get(sid=call.sid).fetch().status == "no-answer":
+            if self.twilio_client.calls.get(sid=call.sid).fetch().status == "busy" or self.twilio_client.calls.get(sid=call.sid).fetch().status == "no-answer":
                 print("CALL ANSWERED! EXITING CALL")
                 return call
             else:
@@ -33,8 +33,19 @@ class Notifier:
 
     def slack_message(self, violation_list: list):
         if len(violation_list) > 0:
-            message = f"Hey bro the following jobs violated the SLA {violation_list}"
+            message = f"Hey bro the following jobs violated the SLA: {violation_list}"
         else:
             message = "The monitor had an unknown problem, please open the ETL_MONITOR job on Jenkins to check."
         self.slack_web_client.chat_postMessage(channel=self.slack_user_monitor_id, text=message)
         print("Slack message sent")
+
+    @staticmethod
+    def notify(violation_list=None):
+        if violation_list is None:
+            violation_list = []
+        notifier = Notifier()
+        notification_method = environ.get("NOTIFICATION_METHOD", default="call").lower()
+        if notification_method.find("call") >= 0:
+            notifier.call()
+        elif notification_method.find("slack") >= 0:
+            notifier.slack_message(violation_list=violation_list)
