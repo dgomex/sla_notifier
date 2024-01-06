9c713d36dbde3ec8ae8f70689cede87f291ce61b
gomes-dani <dgomes@thirtycapital.com>
2024-01-06 22:21:32 +0000

    Change number

--- notifier.py
+++ notifier.py
@@ -6,7 +6,7 @@ from twilio.rest import Client
 class Notifier:
     def __init__(self):
         self.url = "https://handler.twilio.com/twiml/EH3ab3f9548b34cca8c5f6f2c5bc26f5d9"
-        self.phone_number = "+17792329315"
+        self.phone_number = environ.get("SOURCE_PHONE_NUMBER", default="+16786662308")
         self.phone_to_call = environ.get("DESTINATION_PHONE_NUMBER", default="+351913558518")
         self.client = Client()
 
